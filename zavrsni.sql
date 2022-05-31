51ee2c67338bff21f3114cbccd81e3d9c52647c3
Ante Bikić <104563758+abikic7@users.noreply.github.com>
2022-05-31 15:39:47 +0200

    erd2

--- zavrsni.sql
+++ zavrsni.sql
@@ -1,4 +1,5 @@
-# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8  < C:\Users\Bikan\Documents\Završni\zavrsni.sql
+# 
+
 
 drop database if exists zavrsni;
 create database zavrsni default charset utf8mb4;
@@ -12,25 +13,27 @@ create table kupac(
     email varchar(100),
     ulica varchar(50),
     grad varchar(50),
-    narudzba varchar(50),
     drzava varchar(50)
 
 );
 
 create table narudzba(
     sifra int not null primary key auto_increment,
-    kupac varchar(50),
+    kupac int,
     status_narudzbe varchar(50),
-    datum_isporuke datetime
+    datum_isporuke datetime,
+    naruceni_proizvodi int not null
+    
 );
  
 create table naruceni_proizvodi(
     sifra int not null primary key auto_increment,
-    proizvod varchar(50) not null,
+    proizvodi int not null,
     cijena decimal(18,2),
     poštarina decimal(18,2),
     dostava varchar(50),
     naziv_proizvoda varchar(50)
+    
     
 );
 
@@ -38,13 +41,12 @@ create table proizvodi(
     sifra int not null primary key auto_increment,
     naziv_proizvoda varchar(50),
     marka varchar(50),
-    kategorija varchar(50),
     cijena decimal(18,2)
 );
 
 create table kategorija(
     sifra int not null primary key auto_increment,
-    proizvod varchar(50),
+    proizvodi int not null,
     vrsta varchar(50)
 );
 
@@ -53,7 +55,12 @@ create table kategorija(
     naziv varchar(50) not null,
     broj_mobitela int,
     adresa varchar(50),
-    email varchar(100)
+    email varchar(100),
+    narudzba int 
  );
       
- alter table kategorija add foreign key (proizvodi) references proizvodi(sifra);  
+ alter table narudzba add foreign key (kupac) references kupac(sifra);
+  alter table narudzba add foreign key (naruceni_proizvodi) references naruceni_proizvodi(sifra);
+  alter table trgovina add foreign key (narudzba) references narudzba(sifra);  
+   alter table naruceni_proizvodi add foreign key (proizvodi) references proizvodi(sifra);  
+    alter table kategorija add foreign key (proizvodi) references proizvodi(sifra);
